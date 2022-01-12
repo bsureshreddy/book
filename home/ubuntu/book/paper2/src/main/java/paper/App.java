672ff9ff451b7b58107c9c8911272b48479ecbec
root <root@ip-172-31-14-167.us-west-1.compute.internal>
2022-01-12 08:25:41 +0000

    first commit

---
+++ home/ubuntu/book/paper2/src/main/java/paper/App.java
@@ -0,0 +1,13 @@
+package paper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ home/ubuntu/book/paper2/src/test/java/paper/AppTest.java
@@ -0,0 +1,20 @@
+package paper;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
